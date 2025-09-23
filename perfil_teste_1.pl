a8bf31ef2a8f8a5fe1ef3f262a16da0778af33bf
FelipeFerreira44 <felipewbferreira@gmail.com>
2025-09-22 21:44:04 -0300

    Create perfil_teste_1.pl

---
+++ perfil_teste_1.pl
@@ -0,0 +1,18 @@
+% =============================================================================
+% PERFIL 1 - Aluno lógico e analítico.
+Resultado das perguntas deve ser ia e ciência de dados 
+
+% =============================================================================
+
+resposta(1, s).  % logica
+resposta(2, s).  % matematica_estatistica
+resposta(3, s).  % programacao
+resposta(4, n).  % design_visual
+resposta(5, n).  % redes
+resposta(6, n).  % pensamento_critico
+resposta(7, s).  % analise_dados
+resposta(8, n).  % hardware
+resposta(9, n).  % criatividade
+resposta(10, n). % etica
+resposta(11, n). % troubleshooting
+
