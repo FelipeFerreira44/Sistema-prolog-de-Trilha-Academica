fccd5ac727f88b76e3ecb6a63c908017ebbc9c26
FelipeFerreira44 <felipewbferreira@gmail.com>
2025-09-22 21:52:25 -0300

    Create perfil_teste_2.pl

---
+++ perfil_teste_2.pl
@@ -0,0 +1,18 @@
+% =============================================================================
+% PERFIL DE TESTE 2 - Aluno focado em segurança 
+Resultado das perguntas deve indicar os caminhos de Redes e Segurança 
+
+% =============================================================================
+
+resposta(1, n).  % logica
+resposta(2, n).  % matematica_estatistica
+resposta(3, n).  % programacao
+resposta(4, n).  % design_visual
+resposta(5, s).  % redes
+resposta(6, s).  % pensamento_critico
+resposta(7, n).  % analise_dados
+resposta(8, s).  % hardware
+resposta(9, n).  % criatividade
+resposta(10, s). % etica
+resposta(11, s). % troubleshooting
+
