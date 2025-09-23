ee9cc8ac8da2c1974fe8132266335e44c847a50e
FelipeFerreira44 <felipewbferreira@gmail.com>
2025-09-22 22:05:09 -0300

    Create perfil_teste_3.pl

---
+++ perfil_teste_3.pl
@@ -0,0 +1,18 @@
+% =============================================================================
+% PERFIL DE TESTE 3 - Aluno criativo   
+Resultado das perguntas deve indicar o caminho de Desenvolvimento Web
+
+% =============================================================================
+
+resposta(1, n).  % logica
+resposta(2, n).  % matematica_estatistica
+resposta(3, s).  % programacao
+resposta(4, s).  % design_visual
+resposta(5, n).  % redes
+resposta(6, n).  % pensamento_critico
+resposta(7, n).  % analise_dados
+resposta(8, n).  % hardware
+resposta(9, s).  % criatividade
+resposta(10, n). % etica
+resposta(11, n). % troubleshooting
+
